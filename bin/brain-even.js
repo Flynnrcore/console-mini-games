86bcf78e5aff2ba4ce249407ad63d8be2e1ace01
Vladislav Ulyanov <flynnrcore@yandex.ru>
2022-08-04 18:02:56 +0300

    tranfes code of logic game in src/index.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,42 +1,5 @@
-import readlineSync from 'readline-sync';
+#!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
-const nameOfPlayer = readlineSync.question('May I have your name?: ');
-console.log(`Hello, ${nameOfPlayer}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+import evenGames from "../src/games/even-games.js";
 
-const gameEvenNumbers = (num) => {
-  const questionForm = `Question: ${num}`;
-
-  console.log(questionForm);
-
-  const answer = readlineSync.question('Your answer: ');
-
-  const correctAnswer = num % 2 === 0 ? 'yes' : 'no';
-  const opociteAnswer = answer === 'yes' ? 'no' : 'yes';
-
-  if (correctAnswer !== answer) {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${opociteAnswer}'. Let's try again, ${nameOfPlayer}!`);
-    return false;
-  }
-  console.log('Correct!');
-  return true;
-};
-
-const numbersForGame = [15, 6, 7];
-
-const trhreeTries = (arrNumbers) => {
-  let winCount = 1;
-  const congratulations = `Congratulations, ${nameOfPlayer}!`;
-  for (let i = 0; i < arrNumbers.length; i += 1) {
-    if (gameEvenNumbers(arrNumbers[i]) === false) {
-      winCount = 0;
-      break;
-    }
-  }
-  if (winCount === 1) {
-    console.log(congratulations);
-  }
-};
-
-trhreeTries(numbersForGame);
+evenGames;
