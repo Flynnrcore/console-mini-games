51549b0f3293b3e59197daa349aa6437ab2a41dc
Vladislav Ulyanov <flynnrcore@yandex.ru>
2022-08-08 17:30:54 +0300

    reduction of Cognitive Complexity

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,21 +4,20 @@ import generateRandomNumber from '../lib.js';
 const description = 'Find the greatest common divisor of given numbers.';
 
 const getQuestionAndAnswer = () => {
-  const question = `${generateRandomNumber(2, 100)} ${generateRandomNumber(2, 100)}`;
+  const firstNumber = generateRandomNumber(2, 100);
+  const secondNumber = generateRandomNumber(2, 100);
 
-  const getGreatestCommonDivisor = (stringOfNumbers) => {
-    let result = 0;
-    const numbersArr = stringOfNumbers.split(' ');
-    const maxNumber = +numbersArr[0] > +numbersArr[1] ? +numbersArr[0] : +numbersArr[1];
-    for (let i = 1; i <= maxNumber; i += 1) {
-      if (+numbersArr[0] % i === 0 && +numbersArr[1] % i === 0) {
-        result = i;
-      }
+  const question = `${firstNumber} ${secondNumber}`;
+
+  let answer = 0;
+  const maxNumber = firstNumber > secondNumber ? firstNumber : secondNumber;
+  for (let i = 1; i <= maxNumber; i += 1) {
+    if (firstNumber % i === 0 && secondNumber % i === 0) {
+      answer = String(i);
     }
-    return String(result);
-  };
+  }
 
-  const result = [question, getGreatestCommonDivisor(question)];
+  const result = [question, answer];
   return result;
 };
 
