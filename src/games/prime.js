9181cae3d53f55353c10e47001f72115c4246b77
Vladislav Ulyanov <flynnrcore@yandex.ru>
2022-08-08 17:35:16 +0300

    reduction of Cognitive Complexity

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,21 +4,19 @@ import generateRandomNumber from '../lib.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getQuestionAndAnswer = () => {
-  const question = generateRandomNumber(1, 19);
+  const questionNumber = generateRandomNumber(1, 19);
 
-  const isPrime = (number) => {
-    if (number === 1) {
-      return 'no';
+  let answer = 'yes';
+  if (questionNumber === 1) {
+    answer = 'no';
+  }
+  for (let i = 2; i <= questionNumber / 2; i += 1) {
+    if (questionNumber % i === 0) {
+      answer = 'no';
     }
-    for (let i = 2; i <= number / 2; i += 1) {
-      if (number % i === 0) {
-        return 'no';
-      }
-    }
-    return 'yes';
-  };
+  }
 
-  const result = [question, isPrime(question)];
+  const result = [questionNumber, answer];
   return result;
 };
 
